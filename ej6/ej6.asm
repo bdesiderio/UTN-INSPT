45affd853bf87f75ed4386ffc234a17dd2020cf6
Brian Desiderio <bdesiderio@gruposabra.com>
2019-12-05 22:22:49 -0300

    TP 6 funcionando

--- ej6/ej6.asm
+++ ej6/ej6.asm
@@ -24,8 +24,17 @@
 
 section .bss                     ; SECCION DE LAS VARIABLES
 
-numero:
-        resd    1                ; 1 dword (4 bytes)
+numero:         resd    1                ; 1 dword (4 bytes)
+numero1:        resd    1                ; 1 dword (4 bytes)
+numero2:        resd    1                ; 1 dword (4 bytes)
+numero3:        resd    1                ; 1 dword (4 bytes)
+numero4:        resd    1                ; 1 dword (4 bytes)
+numero5:        resd    1                ; 1 dword (4 bytes)
+numero6:        resd    1                ; 1 dword (4 bytes)
+numero7:        resd    1                ; 1 dword (4 bytes)
+numero8:        resd    1                ; 1 dword (4 bytes)
+numero9:        resd    1                ; 1 dword (4 bytes)
+numero10:       resd    1                ; 1 dword (4 bytes)
 
 cadenaSorteo:
         resb    0x0100           ; 256 bytes
@@ -56,8 +65,9 @@ fmtChar:
 fmtLF:
         db    0xA, 0             ; SALTO DE LINEA (LF)
 
+consulteElNro:             db     "Consulte el Nro: " ,0
 ingreseNroGanador:             db     "Ingrese nro ganador: " ,0
-cadenaOrdenada:				db     "0123456789abcdefghijklmnopqrstuvwxyz",0		
+ingresadoEnPosicion:             db     "El nro fue ingresado en la posicion: " ,0
 
 section .text                    ; SECCION DE LAS INSTRUCCIONES
  
@@ -88,8 +98,23 @@ mostrarCadena:                   ; RUTINA PARA MOSTRAR UNA CADENA USANDO PRINTF
         add esp, 8
         ret
 
+mostrarConsulteElNro:                   ; RUTINA PARA MOSTRAR UNA CADENA USANDO PRINTF
+        push consulteElNro
+        push fmtString
+        call printf
+        add esp, 8
+        ret 
+
+
 mostrarIngreseNroGanador:                   ; RUTINA PARA MOSTRAR UNA CADENA USANDO PRINTF
         push ingreseNroGanador
+        push fmtString
+        call printf
+        add esp, 8
+        ret 
+
+mostrarIngresadoEnPosicion:                   ; RUTINA PARA MOSTRAR UNA CADENA USANDO PRINTF
+        push ingresadoEnPosicion
         push fmtString
         call printf
         add esp, 8
@@ -120,15 +145,55 @@ salirDelPrograma:                ; PUNTO DE SALIDA DEL PROGRAMA USANDO EXIT
         call exit
 
 agregarNroSorteo:
+    call mostrarIngreseNroGanador
     call leerNumero
     mov eax,[numero]
-    call mostrarNumero
-    mov [nrosGanadores+edi],eax
-    
-    mov ebx,[nrosGanadores+edi]
-    mov [numero],ebx
-    call mostrarNumero
-    
+    mov [numero1],eax
+
+    call mostrarIngreseNroGanador
+    call leerNumero
+    mov eax,[numero]
+    mov [numero2],eax
+
+    call mostrarIngreseNroGanador
+    call leerNumero
+    mov eax,[numero]
+    mov [numero3],eax
+
+    call mostrarIngreseNroGanador
+    call leerNumero
+    mov eax,[numero]
+    mov [numero4],eax
+
+    call mostrarIngreseNroGanador
+    call leerNumero
+    mov eax,[numero]
+    mov [numero5],eax
+
+    call mostrarIngreseNroGanador
+    call leerNumero
+    mov eax,[numero]
+    mov [numero6],eax
+
+    call mostrarIngreseNroGanador
+    call leerNumero
+    mov eax,[numero]
+    mov [numero7],eax
+
+    call mostrarIngreseNroGanador
+    call leerNumero
+    mov eax,[numero]
+    mov [numero8],eax
+
+    call mostrarIngreseNroGanador
+    call leerNumero
+    mov eax,[numero]
+    mov [numero9],eax
+
+    call mostrarIngreseNroGanador
+    call leerNumero
+    mov eax,[numero]
+    mov [numero10],eax
     ret
 
 _start:
@@ -140,23 +205,109 @@ main:                            ; PUNTO DE INICIO DEL PROGRAMA
     mov edi,0
     mov esi,0
 
-agregar10Nros:
-    call mostrarIngreseNroGanador
     call agregarNroSorteo
-    inc edi
+
+consultarNros:
     call mostrarSaltoDeLinea
-    cmp edi,11
-        je mostrarCadenaYSalir
-    jmp agregar10Nros
+    call mostrarConsulteElNro
+    call leerNumero
+    mov eax,[numero]
 
-mostrarCadenaYSalir:
-    mov esi,0
-
-escribirCadena:
-    mov bx,[nrosGanadores+esi]
-    mov [numero],bx
-    call mostrarNumero
-    inc esi
-    cmp esi,10
+    cmp eax,0
         je salirDelPrograma
-    jmp escribirCadena
+
+    call verificarSiFueIngresado
+
+verificarSiFueIngresado:
+    cmp [numero1],eax
+        je mostrarEnPosicion1
+    cmp [numero2],eax
+        je mostrarEnPosicion2
+    cmp [numero3],eax
+        je mostrarEnPosicion3
+    cmp [numero4],eax
+        je mostrarEnPosicion4
+    cmp [numero5],eax
+        je mostrarEnPosicion5
+    cmp [numero6],eax
+        je mostrarEnPosicion6
+    cmp [numero7],eax
+        je mostrarEnPosicion7
+    cmp [numero8],eax
+        je mostrarEnPosicion8
+    cmp [numero9],eax
+        je mostrarEnPosicion9
+    cmp [numero10],eax
+        je mostrarEnPosicion10
+    call consultarNros
+
+mostrarEnPosicion1:
+    call mostrarIngresadoEnPosicion
+    mov ecx,1
+    mov [numero],ecx
+    call mostrarNumero    
+    jmp consultarNros
+
+mostrarEnPosicion2:
+    call mostrarIngresadoEnPosicion
+    mov ecx,2
+    mov [numero],ecx
+    call mostrarNumero    
+    jmp consultarNros
+
+mostrarEnPosicion3:
+    call mostrarIngresadoEnPosicion
+    mov ecx,3
+    mov [numero],ecx
+    call mostrarNumero    
+    jmp consultarNros
+
+mostrarEnPosicion4:
+    call mostrarIngresadoEnPosicion
+    mov ecx,4
+    mov [numero],ecx
+    call mostrarNumero    
+    jmp consultarNros
+
+mostrarEnPosicion5:
+    call mostrarIngresadoEnPosicion
+    mov ecx,5
+    mov [numero],ecx
+    call mostrarNumero    
+    jmp consultarNros
+
+mostrarEnPosicion6:
+    call mostrarIngresadoEnPosicion
+    mov ecx,6
+    mov [numero],ecx
+    call mostrarNumero    
+    jmp consultarNros
+
+mostrarEnPosicion7:
+    call mostrarIngresadoEnPosicion
+    mov ecx,7
+    mov [numero],ecx
+    call mostrarNumero    
+    jmp consultarNros
+
+mostrarEnPosicion8:
+    call mostrarIngresadoEnPosicion
+    mov ecx,8
+    mov [numero],ecx
+    call mostrarNumero    
+    jmp consultarNros
+
+mostrarEnPosicion9:
+    call mostrarIngresadoEnPosicion
+    mov ecx,9
+    mov [numero],ecx
+    call mostrarNumero    
+    jmp consultarNros
+
+mostrarEnPosicion10:
+    call mostrarIngresadoEnPosicion
+    mov ecx,10
+    mov [numero],ecx
+    call mostrarNumero    
+    jmp consultarNros
+
